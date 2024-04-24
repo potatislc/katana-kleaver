dc26007a679aa8bf4149bcbe62d208cd5b3e1daf
Luc Sommerland <lu1522so-s@student.lu.se>
2024-04-24 21:34:30 +0200

    Added player but no functionality yet

---
+++ sources/player.h
@@ -0,0 +1,18 @@
+#ifndef MY_RAYLIB_GAME_PLAYER_H
+#define MY_RAYLIB_GAME_PLAYER_H
+
+#include <raylib.h>
+#include <ball.h>
+
+struct Player
+{
+    Vector2 position;
+    float speed;
+    float radius;
+};
+
+void moveToPointPlayer(struct Player *player, float x, float y);
+void ballCollisionPlayer(struct Player *player, struct Ball balls[]);
+void drawPlayer(struct Player *player);
+
+#endif
