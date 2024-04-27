fbc3106b93c8b0b2ae7c3735a31ba61bb01c5545
Luc Sommerland <lu1522so-s@student.lu.se>
2024-04-27 09:50:38 +0200

    Pixel perfect low res screen, upscaled

--- sources/main.c
+++ sources/main.c
@@ -2,8 +2,10 @@
 #include "ball.h"
 #include "player.h"
 
-#define SCREEN_WIDTH (1024)
-#define SCREEN_HEIGHT (1024)
+#define SCREEN_WIDTH (1280)
+#define SCREEN_HEIGHT (1280)
+#define VIRTUAL_SCREEN_WIDTH (256)
+#define VIRTUAL_SCREEN_HEIGHT (256)
 
 #define WINDOW_TITLE "Ball Game"
 
@@ -13,16 +15,28 @@ int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
 
-    Vector2 screenCenter = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+    Vector2 vScreenCenter = {VIRTUAL_SCREEN_WIDTH / 2.0f, VIRTUAL_SCREEN_HEIGHT / 2.0f };
+
+    const float virtualRatio = (float)SCREEN_WIDTH/(float)VIRTUAL_SCREEN_WIDTH;
+
+    RenderTexture2D target = LoadRenderTexture(VIRTUAL_SCREEN_WIDTH, VIRTUAL_SCREEN_HEIGHT);
+
+    Camera2D worldSpaceCamera = { 0 };
+    worldSpaceCamera.zoom = 1.0f;
+
+    Rectangle sourceRec = { 0.0f, 0.0f, (float)target.texture.width, -(float)target.texture.height };
+    Rectangle destRec = { -virtualRatio, -virtualRatio, SCREEN_WIDTH + (virtualRatio*2), SCREEN_HEIGHT + (virtualRatio*2) };
+
+    Vector2 origin = { 0.0f, 0.0f };
 
     struct Player player;
-    initPlayer(&player, screenCenter);
+    initPlayer(&player, vScreenCenter);
 
     struct Ball balls[NBR_OF_BALLS];
 
     for (int i = 0; i < sizeof(balls) / sizeof(balls[0]); i++)
     {
-        float testRadius = 64.0f;
+        float testRadius = 32.0f;
         initBall(&balls[i],(Vector2){ testRadius + testRadius * (float)i * 2.0f, testRadius + testRadius * (float)i * 2.0f },testRadius);
     }
 
@@ -58,22 +72,33 @@ int main(void)
 
         // Draw
         //-----------------------------------------------------
+        BeginTextureMode(target);
+            ClearBackground(DARKGRAY);
+
+            // Draw Game World
+            BeginMode2D(worldSpaceCamera);
+                for (int i = 0; i < sizeof(balls) / sizeof(balls[0]); i++)
+                {
+                    drawBall(balls[i], shadowOffset, shadowRadius);
+                }
+
+                drawPlayer(player);
+            EndMode2D();
+        EndTextureMode();
+
+        // Draw to Screen
         BeginDrawing();
+            ClearBackground(DARKGRAY);
 
-        ClearBackground(DARKGRAY);
+            // Draw world camera to screen
+            DrawTexturePro(target.texture, sourceRec, destRec, origin, 0.0f, WHITE);
 
-        for (int i = 0; i < sizeof(balls) / sizeof(balls[0]); i++)
-        {
-            drawBall(balls[i], shadowOffset, shadowRadius);
-        }
+            //DrawText("PRESS SPACE to PAUSE BALL MOVEMENT", 10, GetScreenHeight() - 25, 20, LIGHTGRAY);
 
-        drawPlayer(player);
-        //DrawText("PRESS SPACE to PAUSE BALL MOVEMENT", 10, GetScreenHeight() - 25, 20, LIGHTGRAY);
+            // On pause, we draw a blinking message
+            if (pause && ((framesCounter/30)%2)) DrawText("PAUSED", 350, 200, 30, GRAY);
 
-        // On pause, we draw a blinking message
-        if (pause && ((framesCounter/30)%2)) DrawText("PAUSED", 350, 200, 30, GRAY);
-
-        DrawFPS(10, 10);
+            DrawFPS(10, 10);
 
         EndDrawing();
         //-----------------------------------------------------
