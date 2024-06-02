a2b2e050c5208dead2691b7d9d07406433d385b0
Luc Sommerland <lu1522so-s@student.lu.se>
2024-06-02 13:39:00 +0200

    New texture location

--- sources/asset_loader.c
+++ sources/asset_loader.c
@@ -4,10 +4,10 @@ GameTextures gameTextures;
 
 void LoadGameTextures()
 {
-    gameTextures.floorStandard = LoadTexture("../assets/floor.png");
-    gameTextures.melonBig = LoadTexture("../assets/melon_big.png");
-    gameTextures.melonSmall = LoadTexture("../assets/melon_small.png");
-    gameTextures.melonShadow = LoadTexture("../assets/melon_shadow_big.png");
-    gameTextures.samurai = LoadTexture("../assets/samurai/samurai.png");
-    gameTextures.samuraiShadow = LoadTexture("../assets/samurai/samurai_shadow.png");
+    gameTextures.floorStandard = LoadTexture("../assets/textures/floor.png");
+    gameTextures.melonBig = LoadTexture("../assets/textures/melon/melon_big.png");
+    gameTextures.melonSmall = LoadTexture("../assets/textures/melon/melon_small.png");
+    gameTextures.melonShadow = LoadTexture("../assets/textures/melon/melon_shadow_big.png");
+    gameTextures.samurai = LoadTexture("../assets/textures/samurai/samurai.png");
+    gameTextures.samuraiShadow = LoadTexture("../assets/textures/samurai/samurai_shadow.png");
 }
